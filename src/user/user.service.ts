934e9e053f42c1dd42fb0ff3c32303f1f19d8f33
DanielAzevedocb <daniel.santoscb@gmail.com>
2023-12-06 18:14:38 -0300

    feat: insert migration in project

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dtos/createUser-dto';
-import { UserEntity } from './interfaces/user.entity';
+import { UserEntity } from './entities/user.entity';
 import { hash } from 'bcrypt';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
@@ -17,6 +17,7 @@ export class UserService {
 
     return this.userRepository.save({
       ...createUserDto,
+      typeUser: 1,
       password: passwordHashed,
     });
   }
