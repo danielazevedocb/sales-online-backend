173c94dde1cde26befc46e6dc4ebf2aed93746e3
DanielAzevedocb <daniel.santoscb@gmail.com>
2023-12-23 11:23:01 -0300

    feat: create city controller and insert cache

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,14 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { StateModule } from './state/state.module';
+import { CityModule } from './city/city.module';
+import { AddressModule } from './address/address.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: ['.env.development.local'],
+      envFilePath: ['.env'],
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
@@ -20,6 +23,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       migrationsRun: true,
     }),
     UserModule,
+    StateModule,
+    CityModule,
+    AddressModule,
   ],
   controllers: [],
   providers: [],
---
+++ src/city/city.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CityController } from './city.controller';
+import { CityService } from './city.service';
+import { CityEntity } from './entities/city.entity';
+import { CacheModule } from '@nestjs/cache-manager';
+
+@Module({
+  imports: [
+    CacheModule.register({
+      ttl: 9000000,
+    }),
+    TypeOrmModule.forFeature([CityEntity]),
+  ],
+  controllers: [CityController],
+  providers: [CityService],
+  exports: [CityService],
+})
+export class CityModule {}
